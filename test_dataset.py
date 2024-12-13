1a62d88cd9c22f9a5b19fc25c8a8c95f2d559903
Andrew <AndrewRaineFalkowski@gmail.com>
2024-12-13 16:04:17 -0500

    added run multiple loops and visuals

--- test_dataset.py
+++ test_dataset.py
@@ -2,9 +2,9 @@ import torch
 import numpy as np
 from models import VarSTP, VarGP
 import matplotlib.pyplot as plt
-from utils import TorchStandardScaler, TorchNormalizer
-
-from runners import run_campaign
+from utils import TorchNormalizer
+from runners import run_many_loops
+from visualization import plot_optimization_results
 
 # Load and preprocess data
 data = np.loadtxt("data/P3HT_dataset.csv", delimiter=",", skiprows=1)
@@ -15,21 +15,26 @@ y = torch.tensor(data[:, -1], dtype=torch.double).flatten()
 normalizer = TorchNormalizer()
 X = normalizer.fit_transform(X)
 
-results = run_campaign(
+results = run_many_loops(
     X,
     y,
+    seeds=[1, 25, 323, 42, 5643],
     n_initial=10,
-    n_trials=25,
+    n_trials=90,
     epochs=100,
     learning_rate=0.1,
-    model_class=VarGP,
-    seed=42,
+    model_class=VarSTP,
 )
 
-fig, ax = plt.subplots(dpi=200, figsize=(6, 4))
-trials = np.arange(1, len(results["y_selected"]) + 1)
-ax.plot(trials, results["y_selected"], marker="o", ls="None", color="k")
-ax.plot(trials, np.maximum.accumulate(results["y_selected"]), marker="None", lw=3)
-ax.axhline(torch.max(y), color="k", linestyle="--")
-ax.axvline(10, color="k", linestyle="--")
+# Create figure with subplots
+fig, ax = plt.subplots(1, 2, figsize=(7, 4), dpi=200, width_ratios=[5, 1], sharey=True)
+
+# Use the optimization results plot in the first subplot
+plot_optimization_results(ax[0], results, torch.max(y), n_initial=10)
+
+# Create your second plot
+ax[1].scatter([0] * len(y), y, color="black", marker="o", s=10, alpha=0.5)
+ax[1].set_xticks([])
+
+plt.tight_layout()
 plt.show()
